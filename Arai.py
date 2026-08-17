3f259cb4cc101807ede14031d98384834a0288c7
Green <69070031@kmitl.ac.th>
2026-08-17 15:39:30 +0700

    Update Arai.py

--- Arai.py
+++ Arai.py
@@ -1,8 +1,29 @@
 """555"""
-work = int(input())
-mid = int(input())
-fin = int(input())
-if work >= 5 and mid >= 20 and fin >= 25 :
-    print("pass")
-else :
-    print("fail")
+col,amo = input().split(" ")
+amo = int(amo)
+group = amo // 3
+mod = amo % 3
+R = "Red Green Blue "
+G = "Green Blue Red "
+B = "Blue Red Green "
+if col == "R" :
+    if mod == 2 :
+        print(f"{R * group}Red Green")
+    elif mod == 1 :
+        print(f"{R * group}Red")
+    else :
+        print(R * group)
+elif col == "G" :
+    if mod == 2 :
+        print(f"{G * group}Green Blue")
+    elif mod == 1 :
+        print(f"{G * group}Green")
+    else :
+        print(G * group)
+elif col == "B" :
+    if mod == 2 :
+        print(f"{B * group}Blue Red")
+    elif mod == 1 :
+        print(f"{B * group}Blue")
+    else :
+        print(B * group)
